cbbcb1a3a740671423568c9a880b5200dc8863c9
Christian Berendt <berendt@betacloud-solutions.de>
2020-06-26 18:19:32 +0200

    Enable BuildKit builds

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
-echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
+echo '{ "features": { "buildkit": true }, "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && ( "$TRAVIS_BRANCH" == "master" || -n "$TRAVIS_TAG" ) ]]; then
