bef993475f21fb55bfa93de2f0d1279107344df3
Christian Berendt <berendt@betacloud-solutions.de>
2020-08-06 09:35:58 +0200

    Use trivy as vulnerability scanner (#98)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

---
+++ scripts/security.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Available environment variables
+#
+# REPOSITORY
+# VERSION
+
+VERSION=${VERSION:-latest}
+
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+trivy image --clear-cache
+trivy image $REPOSITORY:$VERSION
