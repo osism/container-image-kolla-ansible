cf9f313b5ab6697497420c9669686471208ecc9d
Christian Berendt <berendt@betacloud-solutions.de>
2020-08-06 12:46:09 +0200

    Add NETBOX_TOKEN environment variable (#99)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

---
+++ files/scripts/secrets.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# source: https://github.com/docker-library/mariadb/blob/master/docker-entrypoint.sh
+#
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+        local var="$1"
+        local fileVar="${var}_FILE"
+        local def="${2:-}"
+        if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+                echo "Both $var and $fileVar are set (but are exclusive)"
+                exit 1
+        fi
+        local val="$def"
+        if [ "${!var:-}" ]; then
+                val="${!var}"
+        elif [ "${!fileVar:-}" ]; then
+                val="$(< "${!fileVar}")"
+        fi
+        export "$var"="$val"
+        unset "$fileVar"
+}
+
+NETBOX_TOKEN_FILE=${NETBOX_TOKEN_FILE:-/run/secrets/NETBOX_TOKEN}
+file_env 'NETBOX_TOKEN'
