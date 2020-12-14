d95a15402f508fc1d9af5a546591460bc18fdfe1
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-14 12:37:12 +0100

    github: add --load
    No output specified for docker-container driver. Build result will
    only remain in the build cache. To push result image into registry
    use --push or to load image into docker use --load
    
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,6 +32,7 @@ else
 fi
 
 docker buildx build \
+    --load \
     --build-arg "OPENSTACK_VERSION=$OPENSTACK_VERSION" \
     --build-arg "UBUNTU_VERSION=$UBUNTU_VERSION" \
     --build-arg "VERSION=$VERSION" \
