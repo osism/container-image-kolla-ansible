7b500edec8b3638c60b772f9ef4bf3709bd875bc
Christian Berendt <berendt@osism.tech>
2021-11-11 20:20:25 +0100

    Use the main branch where necessary (#193)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/push.sh
+++ scripts/push.sh
@@ -19,8 +19,8 @@ COMMIT=$(git rev-parse --short HEAD)
 
 # NOTE: For builds for a specific release, the OpenStack version is taken from the release repository.
 if [[ $VERSION != "latest" ]]; then
-    filename=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/openstack.yml)
-    OPENSTACK_VERSION=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/$filename | grep "openstack_version:" | awk -F': ' '{ print $2 }')
+    filename=$(curl -L https://raw.githubusercontent.com/osism/release/main/$VERSION/openstack.yml)
+    OPENSTACK_VERSION=$(curl -L https://raw.githubusercontent.com/osism/release/main/$VERSION/$filename | grep "openstack_version:" | awk -F': ' '{ print $2 }')
 fi
 
 . defaults/$OPENSTACK_VERSION.sh
