7b57de1322c814f4fc69cafcd109e856a5300d31
Christian Berendt <berendt@osism.tech>
2021-11-17 20:04:52 +0100

    Copy versions.yml to /interface
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/kolla-ansible
+mkdir -p /interface/kolla-ansible /interface/versions
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/kolla-ansible/
+cp /ansible/group_vars/all/versions.yml /interface/versions/kolla-ansible.yml
+
 exec /usr/bin/dumb-init -- "$@"
