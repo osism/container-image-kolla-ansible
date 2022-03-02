d0ceb548e1461f85efb9483276d5e2550755a3f3
Christian Berendt <berendt@osism.tech>
2022-03-02 15:19:41 +0100

    Reclassify the use of overlays
    So far, the overlays have been used to overwrite entire
    parts in the kolla-ansible repository. This is no longer
    needed today.
    
    The overlays are now used to write files to the /interface
    directory to enforce version dependent Ansible parameters.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/kolla-ansible /interface/versions
+mkdir -p /interface/kolla-ansible /interface/versions /interface/overlays
+
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/kolla-ansible/
 cp /ansible/group_vars/all/versions.yml /interface/versions/kolla-ansible.yml
+cp /overlays/kolla-ansible.yml /interface/overlays/kolla-ansible.yml
 
 exec /usr/bin/dumb-init -- "$@"
