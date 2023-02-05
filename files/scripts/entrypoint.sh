4cf15ee1124be075d06d393d56265fd2dd1f0f82
Christian Berendt <berendt@osism.tech>
2023-02-05 12:44:22 +0100

    Render playbooks (#331)
    This script writes a list of all existing playbooks
    to /ansible/playbooks.yml.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/kolla-ansible /interface/versions /interface/overlays
+mkdir -p /interface/kolla-ansible /interface/versions /interface/overlays /interface/playbooks
 
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/kolla-ansible/
 cp /ansible/group_vars/all/versions.yml /interface/versions/kolla-ansible.yml
 cp /overlays/kolla-ansible.yml /interface/overlays/kolla-ansible.yml
+cp /ansible/playbooks.yml /interface/playbooks/kolla-ansible.yml
 
 exec /usr/bin/dumb-init -- "$@"
