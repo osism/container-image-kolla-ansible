587a21b5c0a6c6dda52b2917e6e474a286878994
Christian Berendt <berendt@osism.tech>
2023-06-02 15:44:45 +0200

    Add cryptography to ansible-vault.py script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/ansible-vault.py
+++ files/scripts/ansible-vault.py
@@ -3,10 +3,20 @@
 # NOTE: This is a first step to make Ansible Vault usable via OSISM workers.
 #       It's not ready in that form yet.
 
+import os
+
+from cryptography.fernet import Fernet
 from redis import Redis
 
-redis = Redis(host="redis", port="6379")
-ansible_vault_password = redis.get("ansible_vault_password")
+keyfile = "/share/ansible_vault_password.key"
+if os.path.isfile(keyfile):
+    with open(keyfile, "r") as fp:
+        key = fp.read()
 
-if ansible_vault_password:
-    print(ansible_vault_password.decode("utf-8"))
+    f = Fernet(key)
+
+    redis = Redis(host="redis", port="6379")
+    token = redis.get("ansible_vault_password")
+
+    if token:
+        print(f.decrypt(token).decode("utf-8"))
