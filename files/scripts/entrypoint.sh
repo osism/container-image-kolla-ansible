2d3cd40cd6d828152f49d045f3c2302a5caac0d9
Christian Berendt <berendt@osism.tech>
2023-10-15 19:32:31 +0200

    Add missing then statement (#524)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -7,7 +7,7 @@ cp /ansible/group_vars/all/versions.yml /interface/versions/kolla-ansible.yml
 cp /overlays/kolla-ansible.yml /interface/overlays/kolla-ansible.yml
 cp /ansible/playbooks.yml /interface/playbooks/kolla-ansible.yml
 
-if [[ -e /overlays/release-kolla-ansible.yml  ]];
+if [[ -e /overlays/release-kolla-ansible.yml  ]]; then
     cp /overlays/release-kolla-ansible.yml /interface/overlays/release-kolla-ansible.yml
 else
     rm -f /interface/overlays/release-kolla-ansible.yml
