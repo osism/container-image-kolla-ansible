9d39353874fb3b555264502170a1dc3ecd15c4ea
Jan Horstmann <horstmann@osism.tech>
2025-01-13 12:14:16 +0100

    Fix change.sh for kolla-ansible case
    Modify the change.sh kolla-ansible to actually fetch and checkout the
    specified reference.
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- files/scripts/change.sh
+++ files/scripts/change.sh
@@ -14,8 +14,11 @@ if [[ "$1" == "kolla-ansible" ]]; then
     rm -rf /ansible/roles/*
     rm -rf /repository
 
-    git clone https://opendev.org/openstack/kolla-ansible
-    git pull https://review.opendev.org/openstack/kolla-ansible $1
+    git clone https://opendev.org/openstack/kolla-ansible /repository
+    git -C /repository config advice.detachedHead false
+    git -C /repository fetch https://review.opendev.org/openstack/kolla-ansible.git $2
+    git -C /repository checkout FETCH_HEAD
+
 
     cp -r /repository/ansible/action_plugins/* /ansible/action_plugins
     if [ -e /repository/ansible/filter_plugins ]; then
